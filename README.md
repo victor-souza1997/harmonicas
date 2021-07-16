dc6d291da995e67ee9f957110a9a3b6b5bec8c9b
João Victor <54864686+victor-souza1997@users.noreply.github.com>
2021-07-15 22:24:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# find path by harmonic functions
+ Using Taylor series to solve DPE and get a path towars the objective 
---
+++ main.m
@@ -0,0 +1,78 @@
+
+close all
+clc
+clear
+Mapa = ones(1000,1000);
+
+Mapa(:,1) = 0;
+
+Mapa(:,end) = 0;
+Mapa(1,:) = 0;
+Mapa(end,:) = 0;
+[ny, nx] = size(Mapa);
+cont = 1;
+for j=1:ny
+    for i =1:nx
+      if Mapa(j,i) == 1
+       Mapa(j,i) = cont;
+       cont = cont +1;
+      end
+    end
+end
+Mapa
+
+A = zeros(cont - 1, cont -1);
+for j=1:ny
+    for i =1:nx
+      if Mapa(j,i) != 0
+        idc = Mapa(j,i);
+        
+        A(idc, idc) = -1;
+   
+        if Mapa(j+1, i) !=0
+          A(idc, Mapa(j+1, i)) = 1/4;
+        end
+         if Mapa(j-1, i)!=0
+          A(idc, Mapa(j-1, i)) = 1/4;
+        end
+        if Mapa(j,i+1)!=0
+          A(idc, Mapa(j,i+1)) = 1/4;
+        end
+        if Mapa(j,i-1)!=0
+          A(idc, Mapa(j,i-1)) = 1/4;
+        end
+        
+      end
+    end
+end
+
+%incluir destino
+
+idc = 35;
+A(idc, :) = 0;
+A(idc, idc) = 1;
+d = zeros(length(A),1);
+d(idc) = -1;
+
+%sistema linear de equacoes a resolver  - > A*u=d
+
+u = A\d;
+%colocar o potencial de u para cada uma das posicoes
+count = 1;
+for j=1:ny
+    for i =1:nx
+      if Mapa(j,i) !=0
+       Mapa(j,i) = u(count);
+       count = count +1;
+      end
+    end
+end
+figure(1)
+[Mx, My] = meshgrid(Mapa(:,)
+surf(Mx,My, Mapa)
+[dx, dy] = gradiente(Mapa);
+modulo = sqrt(dx.^2 + dy.^2);
+dxn = - dx./modulo;
+dyn = - dy./modulo;
+
+quiver(1:size(Mapa, 2), 1:size(Mapa,1), dxn, dyn, 0.5);
